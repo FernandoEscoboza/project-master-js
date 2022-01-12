e2857621a8dbe7e5868c5f5e0423b7e5cdbc85f5
Fernando Escoboza <fernandoescoboza21@gmail.com>
2022-01-12 08:42:16 -0400

    Agregando reloj y contacto

---
+++ js/contacto.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $.validate({
+        lang:'es',
+        errorMessagePosition:'error'
+    });
+
+    var theme = $('#theme');
+
+    $('#to-green').click(function(){
+        theme.attr('href','css/green.css');
+    });
+
+    $('#to-red').click(function(){
+        theme.attr('href','css/red.css');
+    });
+
+    $('#to-blue').click(function(){
+        theme.attr('href','css/blue.css');
+    });
+
+    // subir
+    $('.subir').click(function(e){
+        e.preventDefault(); 
+
+        $('html, body').animate({
+            scrollTop:0
+        },500);
+
+        return false;
+    });
+
+
+});
